b2c83188c2218c0f9548e0b1589a40a106f17fc2
Roxana <ms.roxanarahimi@gmail.com>
2025-07-07 15:22:30 +0330

    fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -32,7 +32,8 @@ class ReportController extends Controller
 //            ->havingRaw('COUNT(*) > 1')
 //            ->pluck('AddressID');
 //        return $duplicates;
-        $t = InvoiceAddress::orderByDesc('id')->with('invoices')->whereHasNot('invoices')->paginate(200);
+//        $t = InvoiceAddress::orderByDesc('id')->with('invoices')->whereHas('invoices')->paginate(200);
+        $t = InvoiceAddress::orderByDesc('id')->with('invoices')->doesntHave('invoices')->paginate(200);
         return $t;
         $partIDs = Part::where('Name', 'like', '%نودالیت%')->whereNot('Name', 'like', '%لیوانی%')->whereNot('Name', 'like', '%کیلویی%')->pluck("PartID");
         $storeIDs = Store::orderBy('Code')
