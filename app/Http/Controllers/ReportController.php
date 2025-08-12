a46e958f04468b3133411c9b31c5d224df3ef5f5
Roxana <ms.roxanarahimi@gmail.com>
2025-08-12 12:13:44 +0330

    test

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -26,7 +26,7 @@ class ReportController extends Controller
 {
     public function test(Request $request)
     {
-        $r = DB::connection('sqlsrv2')->table('LGS3.Category')->paginate(20);
+        $r = DB::connection('sqlsrv2')->table('LGS3.Category')->select('CategoryID')->paginate(20);
         return $r;
 //
 
