84ee66576b190507b33a4b009244e2f07a61fecb
Roxana <ms.roxanarahimi@gmail.com>
2025-11-17 23:20:16 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -28,9 +28,7 @@ class ReportController extends Controller
     public function test(Request $request)
     {
 
-        $dat = Address::with('Plant', function ($q) {
-            return $q->with('Store');
-        })->where('Name', 'LIKE', '%' . 'شعبه قزوین' . '%')->paginage(500);
+        $dat = Address::with('Plant')->where('Name', 'LIKE', '%' . 'شعبه قزوین' . '%')->paginage(500);
 
 
         return $dat;
