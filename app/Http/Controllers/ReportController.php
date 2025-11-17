cc98041a89c56247f47d81db7673b60e3b09afdf
Roxana <ms.roxanarahimi@gmail.com>
2025-11-17 22:51:10 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -27,7 +27,7 @@ class ReportController extends Controller
 {
     public function test(Request $request){
 
-          $dat = Store::where('Number','2145549811')->get();
+          $dat = Plant::where('Number','2145549811')->get();
 
           return $dat;
 
