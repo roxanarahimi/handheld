c9f3b1bf189cea02ac9812138acff026022c06eb
Roxana <ms.roxanarahimi@gmail.com>
2025-11-17 23:24:09 +0330

    aa

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -13,7 +13,7 @@ class Address extends Model
 
     public function Plant()
     {
-        return $this->belongsTo(Plant::class,'AddressID', 'AddressRef' );
+        return $this->belongsTo(Plant::class, 'AddressRef','AddressID' );
     }
 
     public function CustomerAddress()
