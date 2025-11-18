04fe88f00f49cced7bddd45d3fec7d8c5f345a75
Roxana <ms.roxanarahimi@gmail.com>
2025-11-19 00:38:01 +0330

    aa

---
+++ app/Models/Broker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Broker extends Model
+{
+    protected $connection = 'sqlsrv';
+    protected $table = 'SLS3.Broker';
+    protected $hidden = ['Version'];
+    use HasFactory;
+}
