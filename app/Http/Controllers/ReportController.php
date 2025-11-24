a387ad8d341623bd1548818d943c991f219713f2
Roxana <ms.roxanarahimi@gmail.com>
2025-11-24 21:48:37 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -35,11 +35,11 @@ class ReportController extends Controller
 {
     public function test(Request $request)
     {
-        $dat0 = InventoryVoucher::  where('Date', '>=', today()->subDays(20))
-            ->whereIn('Number',['39518','38994','39505','39508','39642','39479','39507'])
-//            ->where('InventoryRef', 1)
-//            ->where('State', 2)
-//            ->where('FiscalYearRef', 1405)
+        $dat0 = Order::  where('Date', '>=', today()->subDays(15))
+//            ->whereIn('Number',['39518','38994','39505','39508','39642','39479','39507'])
+            ->where('InventoryRef', 1)
+            ->where('State', 2)
+            ->where('FiscalYearRef', 1405)
 //            ->whereHas('Customer',function ($c){
 //                $c->whereHas('CustomerAddress',function ($a){
 //                    $a->where('Type', 2);
@@ -48,12 +48,12 @@ class ReportController extends Controller
 //        ->whereHas('AssignmentDeliveryItem', function ($p) {
 //            $p->whereHas('Assignment');
 //        })
-//
-//        ->whereHas('OrderItems', function ($q) {
-//            $q->havingRaw('SUM(Quantity) >= ?', [200]);
-//        })
+
+        ->whereHas('OrderItems', function ($q) {
+            $q->havingRaw('SUM(Quantity) >= ?', [200]);
+        })
             ->with('OrderItems')
-//            ->orderBy('OrderID')
+            ->orderBy('OrderID')
             ->get();
         return  $dat0;
 
