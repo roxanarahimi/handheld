b44c5e548722c6a196ea096efc25e8e394ddc387
Roxana <ms.roxanarahimi@gmail.com>
2025-11-25 14:55:37 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -37,7 +37,7 @@ class ReportController extends Controller
     {
         $dat0 = InventoryVoucher::  where('Date', '>=', today()->subDays(15))
 //            ->whereIn('Number',['39518','38994','39505','39508','39642','39479','39507'])
-//            ->where('InventoryRef', 1)
+            ->where('InventoryRef', 10003)
 //            ->where('State', 2)
             ->where('FiscalYearRef', 1405)
 //            ->whereHas('Customer',function ($c){
