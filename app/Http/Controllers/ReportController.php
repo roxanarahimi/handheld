5aca00a30ce4eef5241c162481220a6afc38e77d
Roxana <ms.roxanarahimi@gmail.com>
2025-11-25 15:04:36 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -37,17 +37,19 @@ class ReportController extends Controller
     {
         $dat0 = Order::  where('Date', '>=', today()->subDays(15))
 //            ->whereIn('Number',['39518','38994','39505','39508','39642','39479','39507'])
-            ->where('InventoryRef', 1003)
+//            ->where('InventoryRef', 1)
 //            ->where('State', 2)
             ->where('FiscalYearRef', 1405)
-//            ->whereHas('Customer',function ($c){
-//                $c->whereHas('CustomerAddress',function ($a){
-//                    $a->where('Type', 2);
-//                });
-//            })
-//        ->whereHas('AssignmentDeliveryItem', function ($p) {
-//            $p->whereHas('Assignment');
-//        })
+            ->whereHas('Customer',function ($c){
+                $c->whereHas('CustomerAddress',function ($a){
+                    $a->where('Type', 2);
+                });
+            })
+        ->whereHas('AssignmentDeliveryItem', function ($p) {
+            $p->whereHas('Assignment',function ($d){
+                $d->where('SalesOfficeRef','10003');
+            });
+        })
 
         ->whereHas('OrderItems', function ($q) {
             $q->havingRaw('SUM(Quantity) >= ?', [200]);
