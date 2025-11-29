4a786a0aa9a9e33bfc6e0bf33b83282dec0bb6dc
Roxana <ms.roxanarahimi@gmail.com>
2025-11-29 20:31:19 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -134,7 +134,7 @@ class ReportController extends Controller
         $dat4 = Assignment::orderByDesc('AssignmentID')->first();
         $dat5 = AssignmentDeliveryItem::orderByDesc('AssignmentDeliveryItemID')->first();
 
-        $dat6 = IssuePermit::orderByDesc('IssuePermitID')->first();
+        $dat6 = IssuePermit::where('Number','38993')->first();
         $dat7 = IssuePermitItem::orderByDesc('IssuePermitItemID')->first();
 
 
