86005f7cd099f1672286f1a2ce50f0c8896a10c1
Roxana <ms.roxanarahimi@gmail.com>
2026-02-17 20:32:59 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -59,7 +59,7 @@ class ReportController extends Controller
             ->where('Date', '>=', today()->subDays(7))
             ->where('FiscalYearRef', 1405)
             ->where('InventoryRef', 1)
-//            ->where('State', 2)
+            ->where('State', 2)
 //            ->where('Type', 2)
 
             ->orderByDesc('OrderID')
