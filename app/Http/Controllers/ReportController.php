c2c23c43bc9f72bfe4246dd59f083f2a82c6de33
Roxana <ms.roxanarahimi@gmail.com>
2026-02-17 12:46:03 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -40,7 +40,7 @@ class ReportController extends Controller
     public function test(Request $request)
     {
         $dat = Order::query()
-            ->where('Date', '>=', today()->subDays(20))
+            ->where('Date', '>=', today()->subDays(10))
 
             ->orderByDesc('OrderID')
             ->where('FiscalYearRef',1405)
