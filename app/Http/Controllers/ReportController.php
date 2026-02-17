99037c6766ef764f718d22454e927041849b28a2
Roxana <ms.roxanarahimi@gmail.com>
2026-02-17 21:07:35 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -74,18 +74,19 @@ class ReportController extends Controller
 //            ])
 //            ->paginate(100);
         $dat = Order::query()
-            ->where('Date', '>=', today()->subDays(70))
+            ->where('Date', '>=', today()->subDays(7))
             ->where('FiscalYearRef', 1405)
             ->where('InventoryRef', 1)
-//            ->where('Type', 0)
-//            ->where('State', 2)
+            ->where('Type', 0)
+            ->where('State', 2)
+            ->where('DeliveryMethod', 3)
             ->orderByDesc('OrderID')
             ->whereHas('OrderItems')
             ->whereHas('AssignmentDeliveryItem.Assignment', function ($p) use ($storeIDs, $request) {
                 $p->whereIn('PlantRef', $storeIDs)
-                    ->where('Number', $request['Number'])// 👈 این خط اضافه شد
-                    ->orWhere('Number', $request['n2'])// 👈 این خط اضافه شد
-                    ->orWhere('Number', $request['n3'])// 👈 این خط اضافه شد
+//                    ->where('Number', $request['Number'])// 👈 این خط اضافه شد
+//                    ->orWhere('Number', $request['n2'])// 👈 این خط اضافه شد
+//                    ->orWhere('Number', $request['n3'])// 👈 این خط اضافه شد
                 ;
             })
             ->with([
