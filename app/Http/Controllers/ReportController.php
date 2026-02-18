d3cf80c2a74b3bd5f808a47cf1b471276bec5834
Roxana <ms.roxanarahimi@gmail.com>
2026-02-18 21:55:47 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -79,7 +79,7 @@ class ReportController extends Controller
             ->where('InventoryRef', 1)
 
             ->where('Type', 0)
-            ->where('State', 4)
+            ->where('State', 2)
             ->where('CustomerType', 0)
 //            ->where('DeliveryAddressType', 1)
 
