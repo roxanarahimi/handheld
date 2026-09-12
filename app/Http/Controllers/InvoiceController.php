8222e771148b1081876d65a5ee52bbc52613dea8
Roxana <ms.roxanarahimi@gmail.com>
2026-09-12 16:31:20 +0330

    aa

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -54,7 +54,7 @@ class InvoiceController extends Controller
 //                ->whereNot('Type', 'Order')
                 ->orderByDesc('Type')
 //                ->orderByDesc('OrderNumber')
-                ->take(200)->paginate(100);
+                ->take(200)->get();
 //            $data = InvoiceResource::collection($d3);
 //            return response()->json($d3, 200);
             return response(InvoiceResource::collection($d3), 200);
