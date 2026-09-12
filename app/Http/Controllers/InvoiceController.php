8d145415972511c41e91d2ac417c51fb3971f9b9
Roxana <ms.roxanarahimi@gmail.com>
2026-09-12 15:00:16 +0330

    aa

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -50,7 +50,7 @@ class InvoiceController extends Controller
     {
         try {
             $d3 = Invoice::orderByDesc('id')
-                ->where('DeliveryDate', '>=', today()->subDays(3))
+                ->where('DeliveryDate', '>=', today()->subDays(1))
 //                ->whereNot('Type', 'Order')
                 ->orderByDesc('Type')
 //                ->orderByDesc('OrderNumber')
