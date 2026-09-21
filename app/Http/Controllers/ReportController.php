4fbda29492ea6e87f7e4053f62cdd394780373a4
Roxana <ms.roxanarahimi@gmail.com>
2026-09-21 17:36:05 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -95,20 +95,20 @@ class ReportController extends Controller
                             ->where('FiscalYearRef', 1406)
                             ->where('InventoryRef', 1)
                             ->whereHas('OrderItems', function ($b) {
-                                $b->whereHas('ProductGroupMember', function ($k) {
-                                        $k->whereHas('EntityGroup', function ($q) {
-                                            $q->where('EntityGroupID', "334")
-                                                ->where('EntityGroupingRef', '49');
-                                        });
-                                    })
-                                        ->with([
-                                            'ProductGroupMember' => function ($member) {
-                                                $member->whereHas('EntityGroup', function ($group) {
-                                                    $group->where('EntityGroupID', "334")
-                                                        ->where('EntityGroupingRef', "49");
-                                                })->with('EntityGroup');
-                                            }
-                                        ]);
+//                                $b->whereHas('ProductGroupMember', function ($k) {
+//                                        $k->whereHas('EntityGroup', function ($q) {
+//                                            $q->where('EntityGroupID', "334")
+//                                                ->where('EntityGroupingRef', '49');
+//                                        });
+//                                    })
+//                                        ->with([
+//                                            'ProductGroupMember' => function ($member) {
+//                                                $member->whereHas('EntityGroup', function ($group) {
+//                                                    $group->where('EntityGroupID', "334")
+//                                                        ->where('EntityGroupingRef', "49");
+//                                                })->with('EntityGroup');
+//                                            }
+//                                        ]);
                                 $b->where('Quantity', '>=', 100);
                             })
                             ->where('State', 2);
