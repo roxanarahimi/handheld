947321b586d2b9d09048d5fa33724518679d9668
Roxana <ms.roxanarahimi@gmail.com>
2026-09-21 17:11:20 +0330

    aa

--- app/Models/ProductGroupMember.php
+++ app/Models/ProductGroupMember.php
@@ -18,6 +18,6 @@ class ProductGroupMember extends Model
     }
     public function EntityGroup()
     {
-        return $this->hasOne(EntityGroup::class, 'EntityGroupID','GroupRef');
+        return $this->hasOne(EntityGroup::class, 'EntityGroupingRef','GroupRef');
     }
 }
