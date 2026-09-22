0678088ddf65026066b074d4d792b9cb53f3f515
Roxana <ms.roxanarahimi@gmail.com>
2026-09-22 11:32:59 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -47,12 +47,12 @@ class ReportController extends Controller
 //          $we = EntityGroup::where('EntityGroupID',334)->where('EntityGroupingRef',49)->first();
 //          $f = ProductGroupMember::all()->first();
 //          return [$we,$f];
-            $p = Product::where('Number','7010302351')
+            $p = Product::
 //                ->whereHas('ProductGroupMember', function ($k) {
 //                $k->where('GroupRef','334');
 //            })
-                ->with('ProductGroupMember')
-                ->get();
+                with('ProductGroupMember')
+                ->first();
 //            return $p;
             $storeIDs = Plant::orderBy('PlantID')
                 ->where(function ($query) {
