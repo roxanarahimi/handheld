6322907f162e6ce5589fa336a4deaa194203a012
Roxana <ms.roxanarahimi@gmail.com>
2026-09-22 11:39:14 +0330

    aa

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -73,19 +73,19 @@ class ReportController extends Controller
                     $s->where('State', 2)
                         ->orWhere('State', 3);
                 })
-                ->where('Date', '>=', today()->subDays(5))
+                ->where('Date', '>=', today()->subDays(10))
                 ->orderByDesc('AssignmentID')
                 ->whereIn('PlantRef', $storeIDs)
-                ->has('AssignmentDeliveryItem', '=', 1)
+//                ->has('AssignmentDeliveryItem', '=', 1)
                 ->whereHas('AssignmentDeliveryItem', function ($q) {
                     $q->whereHas('Order', function ($t) {
-                        $t->where('Date', '>=', today()->subDays(5))
+                        $t->where('Date', '>=', today()->subDays(10))
                             ->where('FiscalYearRef', 1406)
                             ->where('InventoryRef', 1)
                             ->whereHas('OrderItems', function ($b) {
-                                $b->whereHas('ProductGroupMember', function ($k) {
-                                    $k->where('GroupRef','334');
-                                });
+//                                $b->whereHas('ProductGroupMember', function ($k) {
+//                                    $k->where('GroupRef','334');
+//                                });
                                 $b->where('Quantity', '>=', 100);
                             })
                             ->where('State', 2);
